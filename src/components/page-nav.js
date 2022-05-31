2b1f29552d814b53ed81eda7932906d33059167c
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-06-01 00:11:21 +0100

    mobile nav

--- src/components/page-nav.js
+++ src/components/page-nav.js
@@ -8,50 +8,50 @@ import { BiChat } from 'react-icons/bi';
 
 export default function PageNav() {
   return (
-    <div className="page-nav text-cyan-100 text-lg flex flex-col w-max h-screen justify-center px-6 gap-12 border-r-2 border-y-0 border-l-0 border-r-teal-500">
+    <div className="page-nav hidden text-cyan-100 lg:flex flex-col w-max h-screen justify-center px-4 gap-8 border-r-2 border-y-0 border-l-0 border-r-teal-500">
       <NavLink to={'/'}
         className="page-nav__item flex flex-col justify-center items-center rounded-full p-4"
         style={({ isActive }) =>
           isActive ? { backgroundColor: "rgb(248,250,252, 0.05)"} : undefined
         }
       >
-        <RiHome2Line className="text-teal-500 text-4xl" />
+        <RiHome2Line className="text-teal-500 text-xl" />
         {/* <span>Home</span> */}
       </NavLink>
-      <NavLink to={'/'}
+      <NavLink to={'/about'}
         className="page-nav__item flex flex-col justify-center items-center rounded-full p-4"
         style={({ isActive }) =>
           isActive ? { backgroundColor: "rgb(248,250,252, 0.05)"} : undefined
         }
       >
-        <BsPerson className="text-teal-500 text-4xl" />
+        <BsPerson className="text-teal-500 text-xl" />
         {/* <span>About</span> */}
       </NavLink>
-      <NavLink to={'/'}
+      <NavLink to={'/projects'}
         className="page-nav__item flex flex-col justify-center items-center rounded-full p-4"
         style={({ isActive }) =>
           isActive ? { backgroundColor: "rgb(248,250,252, 0.05)"} : undefined
         }
       >
-        <FiGrid className="text-teal-500 text-4xl" />
+        <FiGrid className="text-teal-500 text-xl" />
         {/* <span>Work</span> */}
       </NavLink>
-      <NavLink to={'/'}
+      <NavLink to={'/writings'}
         className="page-nav__item flex flex-col justify-center items-center rounded-full p-4"
         style={({ isActive }) =>
           isActive ? { backgroundColor: "rgb(248,250,252, 0.05)"} : undefined
         }
       >
-        <HiOutlinePencilAlt className="text-teal-500 text-4xl" />
+        <HiOutlinePencilAlt className="text-teal-500 text-xl" />
         {/* <span>Writings</span> */}
       </NavLink>
-      <NavLink to={'/'}
+      <NavLink to={'/contact'}
         className="page-nav__item flex flex-col justify-center items-center rounded-full p-4"
         style={({ isActive }) =>
           isActive ? { backgroundColor: "rgb(248,250,252, 0.05)"} : undefined
         }
       >
-        <BiChat className="text-teal-500 text-4xl" />
+        <BiChat className="text-teal-500 text-xl" />
         {/* <span>Contact</span> */}
       </NavLink>
     </div>
--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -2,17 +2,17 @@ import React from "react";
 
 export default function Welcome() {
   return (
-    <div className="px-32 text-slate-50 text-2xl relative h-screen flex flex-col overflow-hidden font-merri leading-10 tracking-wide ">
-      <div className="w-3/4">
-        <h1 className="mb-4 mt-40"><span className="animate-pulse pr-4 text-4xl">👋🏾</span>Hi, my name is Sodiq.</h1>
-        <p>
-          I’m a full-stack software developer based in Lagos, NG. <br />
-          I’m passionate about education, and one of my life-long goals is to travel around the world teaching.<br />
-          I spend a lot of time perfecting my tech &amp; interpersonal skills to give out the best and create a great experience for whoever I work/meet with. I have an excellent eye for design and I pay very keen attention to details.<br />
-          <span className="mt-8">You can read about my story <span className="underline decoration-teal-500 decoration-4">here</span>, or check out my most recent <span className="underline decoration-teal-500 decoration-4">projects</span>.</span>
+    <div className="px-6 lg:px-14 text-slate-50 relative h-screen flex flex-col justify-center overflow-auto lg:justify-start lg:overflow-hidden font-lato leading-7 lg:leading-10 tracking-wide text-base md:text-lg">
+      <div className="w-4/4 md:w-3/4">
+        <h1 className="mb-4 lg:mt-36"><span className="animate-pulse pr-4 text-4xl">👋🏽</span>Hi, my name is <span className="font-bold text-lg md:text-2xl">Sodiq</span>.</h1>
+        <p className="lg:pl-4 font-light">
+          A full-stack software developer in Lagos, NG and I am passionate about education. <br />
+          One of my life-long goals is to travel around the world teaching from my <span className="underline decoration-teal-500 decoration-4 font-normal">experience</span>.<br />
+          I spend a lot of time perfecting my tech &amp; interpersonal skills to give out the best and create a great experience for whoever I work/meet with. I have an excellent eye for design and I pay very keen attention to details.
         </p>
+        <p className="mt-8 lg:pl-4 font-light">You can read about my story <span className="underline decoration-teal-500 decoration-4 font-normal">here</span>, or check out my most recent <span className="underline decoration-teal-500 decoration-4 font-normal">projects</span>.</p>
       </div>
-      <div className="text-big absolute z-1 bottom-32 right-0 opacity-5 font-black font-elsie">
+      <div className="text-big absolute z-1 bottom-32 right-0 opacity-5 font-black font-elsie hidden lg:block">
         Hello
       </div>
     </div>
