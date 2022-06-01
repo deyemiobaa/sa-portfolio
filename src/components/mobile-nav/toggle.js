de57bee88575e70eab892704f1bcb5d3f955aba6
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-06-02 00:14:38 +0100

    fink icon link accessibility

--- src/components/mobile-nav/toggle.js
+++ src/components/mobile-nav/toggle.js
@@ -5,7 +5,7 @@ export default function Toggle({ toggle, element }) {
     <button className="mobile-nav__toggle flex bg-slate-100 flex-col justify-center items-center rounded-full p-4 w-max absolute top-0 left-0 z-20"
       onClick={toggle}
     >
-     {element}
+      {element}
     </button>
   )
 }
