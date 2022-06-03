f5a7ab2ea680fa4da46beeb152f880f020f0f695
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-06-03 17:16:37 +0100

    fix icons scale

--- src/components/icons.js
+++ src/components/icons.js
@@ -10,35 +10,35 @@ export default function SocialIcons() {
         target="_blank"
         rel="noopener noreferrer"
       >
-        <FiMail className="text-teal-500 text-lg hover:scale-[1.01] hover:transition-transform hover:ease-in-out hover:duration-[300ms]" />
+        <FiMail className="text-teal-500 text-lg hover:scale-[1.5] hover:transition-transform hover:ease-in-out hover:duration-[300ms]" />
       </a>
       <a
         href="https://github.com/deyemiobaa?tab=repositories"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <FiGithub className="text-teal-500 text-lg hover:scale-[1.01] hover:transition-transform hover:ease-in-out hover:duration-[300ms]" />
+        <FiGithub className="text-teal-500 text-lg hover:scale-[1.5] hover:transition-transform hover:ease-in-out hover:duration-[300ms]" />
       </a>
       <a
         href="https://www.linkedin.com/in/sodiqa/"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <FiLinkedin className="text-teal-500 text-lg hover:scale-[1.01] hover:transition-transform hover:ease-in-out hover:duration-[300ms]" />
+        <FiLinkedin className="text-teal-500 text-lg hover:scale-[1.5] hover:transition-transform hover:ease-in-out hover:duration-[300ms]" />
       </a>
       <a
         href="https://twitter.com/deyemiobaa"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <FiTwitter className="text-teal-500 text-lg hover:scale-[1.01] hover:transition-transform hover:ease-in-out hover:duration-[300ms]" />
+        <FiTwitter className="text-teal-500 text-lg hover:scale-[1.5] hover:transition-transform hover:ease-in-out hover:duration-[300ms]" />
       </a>
       <a
         href="https://lichess.org/@/sodiq90"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <SiLichess className="text-teal-500 text-lg hover:scale-[1.01] hover:transition-transform hover:ease-in-out hover:duration-[300ms]" />
+        <SiLichess className="text-teal-500 text-lg hover:scale-[1.5] hover:transition-transform hover:ease-in-out hover:duration-[300ms]" />
       </a>
     </>
   )
