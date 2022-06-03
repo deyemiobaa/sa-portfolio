36a197d337a195282e89da13b4bd9c17c783c600
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-06-03 02:34:25 +0100

    fixed nav position

--- src/components/mobile-nav/nav.js
+++ src/components/mobile-nav/nav.js
@@ -12,10 +12,10 @@ export default function MobileNav() {
   };
   return (
     <div
-      className="mobile-nav absolute z-20 h-screen w-3/4 md:w-3/5 lg:hidden font-lato transition-all ease-in-out duration-1000"
+      className="mobile-nav fixed inset-0 z-20 w-3/4 md:w-3/5 lg:hidden transition-all ease-in-out duration-1000"
       style={isOpen ? undefined : { "width": "0" }}
     >
-      {!isOpen && <Toggle style={{ "backgroundColor": "rgba(248, 250, 252, 0.05)" }} toggle={() => toggleNav()} element={<CgMenu className="text-2xl text-teal-500 font-bold" />} />}
+      {!isOpen && <Toggle style={{ "backgroundColor": "#0f172a" }} toggle={() => toggleNav()} element={<CgMenu className="text-2xl text-teal-500 font-bold" />} />}
       {isOpen && <Toggle toggle={() => toggleNav()} element={<CgClose className="text-2xl text-neutral-900 font-bold" />} />}
       <Navigation
         handleClickProp={() => toggleNav()}
