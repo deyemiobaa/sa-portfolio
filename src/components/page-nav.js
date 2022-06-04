e1ef336cf6e6ff06fbe57f65140787f2960415b3
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-06-04 23:15:24 +0100

    reduce animations

--- src/components/page-nav.js
+++ src/components/page-nav.js
@@ -30,12 +30,12 @@ export default function PageNav() {
     }
   ]
   return (
-    <div className="page-nav hidden lg:flex flex-col w-max h-screen justify-center px-4 gap-6 text-teal-500 text-xl fixed left-0 bottom-0 top-0 z-20">
+    <div className="page-nav hidden lg:flex flex-col w-max h-screen justify-center px-4 gap-6 text-slate-50 text-xl fixed left-0 bottom-0 top-0 z-20">
       {elements.map((element, index) => 
         <NavLink to={element.path}
-          className="page-nav__item flex flex-col justify-center items-center rounded-full p-4 hover:bg-black/50 hover:scale-[1.1] hover:transition-transform hover:ease-in-out hover:duration-[300ms] transition-transform hover:animate-pulse"
+          className="page-nav__item flex flex-col justify-center items-center rounded-full p-4 hover:bg-black/50 hover:scale-[1.1] hover:transition-transform hover:ease-in-out hover:duration-[300ms]"
           style={({ isActive }) =>
-            isActive ? { backgroundColor: "rgb(248,250,252, 0.05)", borderRight: "2px solid #14b8a6" } : undefined
+            isActive ? { backgroundColor: "rgb(248,250,252, 0.05)", borderRight: "2px solid #14b8a6", color: "#14b8a6" } : undefined
           }
           key={index}
         >
