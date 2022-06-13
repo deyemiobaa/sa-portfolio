fb3c15a29b94c9dc0237eec8c3d368b54d735381
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-06-14 00:32:56 +0100

    touch ups on big font

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import Project from "../components/project";
-import {projects} from "../assets/data/data";
+import { projects } from "../assets/data/data";
 
 export default function Projects() {
   useEffect(() => {
@@ -12,23 +12,26 @@ export default function Projects() {
   }, []);
 
   return (
-    <div className="relative px-6 leading-7 lg:leading-10 tracking-wide mt-28 md:mt-36">
+    <div className="px-6 leading-7 lg:leading-10 tracking-wide mt-28 md:mt-36">
       <div className="w-4/4 md:w-3/4 mx-auto mb-10 static z-10">
-        <h1 className="mb-10 text-4xl md:text-5xl bg-clip-text text-transparent bg-gradient-to-r from-emerald-500 to-amber-500 font-bold">Projects . Open-Source</h1>
-        <p className="font-bold">
-          Building is fun!
-        </p>
-        <p className="mt-8 font-light">Building projects is something I really enjoy doing. Apart from the fresh experiences I gain with each project, the thrill of seeing a project through from beginning to end and having it work as intended makes the process/hurdles interesting.
-        </p>
-        <p className="font-light mb-36 md:mb-80"><span className="button underline decoration-teal-500 decoration-4 font-normal cursor-pointer">Below</span> are some of my most recent works. Some are finished, in progress, personal, and open-sourced.</p>
+        <div className="relative h-screen">
+          <h1 className="mb-10 text-4xl md:text-5xl bg-clip-text text-transparent bg-gradient-to-r from-emerald-500 to-amber-500 font-bold">Projects . Open-Source</h1>
+          <p className="font-bold">
+            Building is fun!
+          </p>
+          <p className="mt-8 font-light">Building projects is something I really enjoy doing. Apart from the fresh experiences I gain with each project, the thrill of seeing a project through from beginning to end and having it work as intended makes the process/hurdles interesting.
+          </p>
+          <p className="font-light"><span className="button underline decoration-teal-500 decoration-4 font-normal cursor-pointer">Below</span> are some of my most recent works. Some are finished, in progress, personal, and open-sourced.
+          </p>
+          <div className="text-big absolute z-1 bottom-44 left-0 opacity-[0.02] font-black font-elsie hidden lg:block">
+            Build
+          </div>
+        </div>
         <div id="projects-section" className="w-full grid grid-cols-1 md:grid-cols-2 gap-8 text-center">
           {projects.map(project =>
             <Project key={project.id} title={project.title} description={project.description} technologies={project.technologies} link={project.url} image={project.image} />
           )}
         </div>
-      </div>
-      <div className="text-big fixed z-1 bottom-32 -right-10 opacity-[0.02] font-black font-elsie hidden lg:block">
-        Build
       </div>
     </div>
   );
--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -11,13 +11,13 @@ export default function Welcome() {
           <h1 className="mb-4 font-bold text-4xl md:text-8xl">Hi,<br /> I'm Sodiq,<br />Software developer.</h1>
           <p>I'm a Front-end Developer, ready to build with your team<span className="pl-4 text-2xl">🚀</span>.</p>
           <button type="button" className="border px-8 mt-4 border-teal-500 bg-transparent hover:bg-teal-500 hover:border ease-in-out duration-700 transition-colors text-teal-500 hover:text-black font-bold w-max sticky z-[11]">Hire me</button>
-          <div className="text-big absolute z-1 left-0 bottom-32 opacity-[0.02] font-black font-elsie hidden lg:block">
+          <div className="text-big absolute z-1 left-0 bottom-20 opacity-[0.02] font-black font-elsie hidden lg:block">
             Hello
           </div>
         </div>
         <div>
-          <h3 className="text-xl font-normal mb-4">If you need:</h3>
-          <ul className="md:w-3/4">
+          <h3 className="text-xl font-normal">If you need:</h3>
+          <ul className="md:w-3/4 my-4">
             {skills.map(skill =>
               <li key={skill.id} className="flex items-center gap-4 py-4 pr-2">
                 <div className="text-xl text-teal-500 bg-[#0d1117] p-4 rounded-full">{skill.icon}</div>
@@ -25,11 +25,10 @@ export default function Welcome() {
               </li>
             )}
           </ul>
-          <h3 className="text-xl font-normal mt-4">You're in the right place.</h3>
-          <div>
-            <h3>Skills &amp; Tools</h3>
+          <h3 className="text-xl font-normal">You're in the right place.</h3>
+          <div className="mb-4 mt-6">
             <p>Here is a current list of the technologies &amp; tools I use regularly.</p>
-            <ul className="grid grid-cols-2 gap-x-4 md:w-3/5 text-[15px] list-[square] ml-4 my-8">
+            <ul className="grid grid-cols-2 gap-x-4 md:w-3/5 text-[15px] list-[square] ml-4 mb-8 mt-6">
               {techSkills.map(skill =>
                 <li key={skill.id} className="pl-px">{skill.skill}</li>
               )}
