39878f2e8e33b9d53b677131175d1f685d144985
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-06-13 23:19:39 +0100

    replace background colors

--- src/components/project.js
+++ src/components/project.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Project({ title, description, image, technologies, link }) {
   return (
-    <a href={link} target="_blank" rel="noopener noreferrer" className="flex flex-col items-center rounded-lg bg-slate-900/75 shadow-lg hover:scale-[1.01] hover:transition-transform hover:ease-in-out hover:duration-[300ms] static z-10">
+    <a href={link} target="_blank" rel="noopener noreferrer" className="flex flex-col items-center rounded-lg bg-slate-900/25 shadow-lg hover:scale-[1.01] hover:transition-transform hover:ease-in-out hover:duration-[300ms] static z-10">
       <img className="w-max-full h-auto object-cover" src={image} alt={title} />
       <div>
         <div className="text-2xl p-4 mt-2">{title}</div>
