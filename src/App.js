11c571b99f485a4df4edabf90e30fce3a2bdd1a7
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-06-25 01:05:37 +0100

    remove opacity from bg

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default function App() {
   }, [state])
 
   return (
-    <div className="w-full min-h-screen bg-[#08070b]/[0.97] relative grid font-lato text-[#e4f0fa8c] text-base md:text-lg">
+    <div className="w-full min-h-screen bg-[#08070b] relative grid font-lato text-[#e4f0fa8c] text-base md:text-lg">
       <PageNav />
       <MobileNav />
       <Routes>
