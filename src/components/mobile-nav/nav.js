d0f0b7ab8611b737772e1341cb3bb86925c67a95
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-14 14:08:08 +0100

    change toggle

--- src/components/mobile-nav/nav.js
+++ src/components/mobile-nav/nav.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import Navigation from "./navigation";
 import Toggle from "./toggle";
-import { CgMenu, CgClose } from "react-icons/cg";
-
 
 export default function MobileNav() {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,8 +13,7 @@ export default function MobileNav() {
       className="fixed inset-0 z-20 w-3/4 transition-all duration-1000 ease-in-out mobile-nav md:w-3/5 lg:hidden"
       style={isOpen ? undefined : { "width": "0" }}
     >
-      {!isOpen && <Toggle style={{ "backgroundColor": "#08070b" }} toggle={() => toggleNav()} element={<CgMenu className="text-2xl font-bold text-teal-500" />} />}
-      {isOpen && <Toggle toggle={() => toggleNav()} element={<CgClose className="text-2xl font-bold text-neutral-900" />} />}
+      <Toggle toggle={isOpen} handleToggle={() => toggleNav()} />
       <Navigation
         handleClickProp={() => toggleNav()}
         style={isOpen ? { "width": "100%", "opacity": "1" } : { "width": "0%", "opacity": "0" }}
--- src/components/mobile-nav/toggle.js
+++ src/components/mobile-nav/toggle.js
@@ -1,13 +1,19 @@
 import React from "react";
 
-export default function Toggle({ toggle, element, style }) {
+export default function Toggle({ handleToggle, toggle }) {
   return (
-    <button className="mobile-nav__toggle flex bg-slate-100 flex-col justify-center items-center rounded-full p-4 w-max absolute top-0 left-0 z-20 animate-spin-once backdrop-grayscale"
-      onClick={toggle}
-      style={style}
-      aria-label="Toggle mobile navigation"
-    >
-      {element}
-    </button>
+    <div
+      className="absolute top-0 left-0 z-20 flex flex-col justify-between w-5 h-4 mt-5 ml-5"
+      onClick={handleToggle}>
+      <span
+        className={`w-full h-0.5 transition-transform duration-500 origin-[0%_0%] rounded-xl ${toggle ? "bg-neutral-900" : "bg-teal-500"}`}
+        style={toggle ? { transform: "rotate(45deg)" } : {}}></span>
+      <span
+        className="w-3/4 h-0.5 transition-opacity duration-200 rounded-xl bg-teal-500"
+        style={toggle ? { opacity: 0 } : {}}></span>
+      <span
+        className={`w-full h-0.5 transition-transform duration-500 origin-[0%_150%] rounded-xl ${toggle ? "bg-neutral-900" : "bg-teal-500"}`}
+        style={toggle ? { transform: "rotate(-45deg)" } : {}}></span>
+    </div>
   )
 }
