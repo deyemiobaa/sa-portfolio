2f0c01cb0343bcd0647c20dfdab02b0c22da7265
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-10-05 21:37:39 +0100

    convert to SPA

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import useTitle from "./components/useTitle";
-import PageNav from "./components/page-nav";
 import SocialNav from "./components/social-nav";
 import MobileNav from "./components/mobile-nav/nav";
 import Welcome from "./pages/welcome";
 import Contact from "./pages/contact";
-import Projects from "./pages/projects";
 import MobileSocialNav from "./components/mobile-social-nav";
-import About from "./pages/about";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -33,14 +30,13 @@ export default function App() {
 
   return (
     <div className="w-full min-h-screen bg-[#08070b] relative grid font-lato text-[#d4d4d4] text-base md:text-lg">
-      <PageNav />
       <MobileNav />
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <main className="px-24 leading-7 tracking-wide lg:leading-10">
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
       <SocialNav />
       <MobileSocialNav />
     </div>
