e3584de29f3afc4b433c0a152a38b8ab13699251
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-10-05 21:38:03 +0100

    improve project size and details

--- src/components/project.js
+++ src/components/project.js
@@ -1,22 +1,17 @@
 import React from "react";
 
-export default function Project({ title, description, image, technologies, link }) {
+export default function Project({ title, description, image, link }) {
   return (
-    <div className="flex flex-col rounded-lg bg-slate-900/50 shadow-lg hover:scale-[1.01] hover:transition-transform hover:ease-in-out hover:duration-[300ms] static z-10" data-aos="zoom-out">
-      <img className="object-cover h-auto w-max-full" src={image} alt={title} />
-      <div className="px-4">
-        <a href={link} target="_blank" rel="noopener noreferrer" >
-          <div className="p-4 mt-2 text-2xl underline">{title}</div>
-        </a>
-        <div className="px-4 mb-4 text-left">
+    <div className="flex flex-col max-w-sm shadow-lg" data-aos="fade-up">
+      <img className="object-cover h-auto" src={image} alt={title} />
+      <div>
+        <h3 className="py-2 my-4 text-2xl font-medium text-white">{title}</h3>
+        <div className="py-2 text-base">
           {description}
         </div>
-        <div>Built With:</div>
-        <div className="flex flex-wrap justify-center gap-6 p-4">
-          {technologies.map(tech =>
-            <img src={tech.image} style={{ "height": "36px", "width": "36px" }} alt={tech.name} key={tech.name} title={tech.name} />)
-          }
-        </div>
+        <a href={link} target="_blank" rel="noopener noreferrer" className="text-sm font-semibold text-white hover:text-teal-300 animate-pulse hover:animate-none">
+          VIEW PROJECT &gt;
+        </a>
       </div>
     </div>
   )
