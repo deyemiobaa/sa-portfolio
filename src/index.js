ed19cd3e7923b1d7e4ec846d813900a3fec4da8f
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-10-29 12:08:40 +0100

    add tracking analytics

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import ScrollToTop from './components/scrollToTop';
+import { inject } from '@vercel/analytics';
 import App from './App';
 import './index.css';
+
+inject();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
